65ee4a7f0ace499ef7e7c082d940a06f182b005f
Javi Ortiz <javiortizinthewall@gmail.com>
2024-07-22 14:50:39 +0200

    test changing the update.sh script

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Navigate to the directory containing your repository
-cd /home/orangepi/cochesNet-GTE
+cd /home/orangepi
 
 # Pull the latest changes from the Git repository
-git pull
+gh repo clone JaviiOrtiiz/cochesNet-GTE
+
+# Navigate to the repository
+cd cochesNet-GTE
 
 # Stop and remove the old container if it exists
 if [ "$(docker ps -q -f name=cochesnet-GTE)" ]; then
